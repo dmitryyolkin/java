c7aac3bb432456480a34addb745e62cf1fe4fd02
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2016-02-24 08:05:01 +0400

    resteasy module
    * web-xml setup
    * simple GET example

--- rest/resteasy/src/main/java/ResteasyMessageService.java
+++ rest/resteasy/src/main/java/ResteasyMessageService.java
@@ -1,6 +1,8 @@
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
@@ -16,6 +18,12 @@ public class ResteasyMessageService {
         //potentially we can return what we need (String, POJO if we support serialization)
         //the same as for Jersey
         return Response.status(200).entity(String.format("Msg: %s", msg)).build();
+    }
+
+    @GET
+    @Produces({MediaType.TEXT_PLAIN})
+    public Response printMessages(){
+        return Response.status(200).entity("No messages: use /message/{m} path to see result").build();
     }
 
 }
