8db91bb241bf39c3a4bc1218401bd91280333118
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2016-05-01 19:46:30 +0300

    add rabbitmq in dependencies

---
+++ rabbitmq/src/main/java/Producer.java
@@ -0,0 +1,29 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by yolkin on 01.05.2016.
+ * It's test class to check sending messages to rabbitmq
+ */
+public class Producer {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+}
