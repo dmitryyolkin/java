c018c4e98e1b4def92ca9a0133b66a55f67b4d63
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2017-04-12 08:06:01 +0400

    * add empty beans.xml * tune pom.xml

---
+++ jee/bookservice/src/main/java/com/yolkin/bookservice/Main.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2008-2017 Maxifier Ltd. All Rights Reserved.
+ */
+package com.yolkin.bookservice;
+
+/**
+ * @author dmitry.yolkin (dmitry.yolkin@maxifier.com) (2017-04-12 07:55)
+ */
+public class Main {
+}
