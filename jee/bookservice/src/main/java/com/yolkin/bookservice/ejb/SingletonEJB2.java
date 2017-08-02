8a3283ebc3ba54b3fccf8471d99334d2537d5533
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2017-08-02 07:37:35 +0400

    EJB Singleton * Startup * DependsOn

---
+++ jee/bookservice/src/main/java/com/yolkin/bookservice/ejb/SingletonEJB2.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2008-2017 Maxifier Ltd. All Rights Reserved.
+ */
+package com.yolkin.bookservice.ejb;
+
+import javax.ejb.DependsOn;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+/**
+ * SingletonEJB2
+ *
+ * @author dmitry.yolkin (dmitry.yolkin@maxifier.com) (2017-07-26 08:08)
+ */
+@Singleton
+@DependsOn("SingletonEJB1")
+@Startup
+public class SingletonEJB2 {
+}
