f1ed468e2d10e9e1b1d8b05f0fba55c4c84841c9
dmitry yolkin <dmitry.yolkin@gmail.com>
2018-01-11 08:16:42 +0400

    JMSProducer with JMSContext

--- jee/bookservice/app/app-service/src/main/java/com/book/jms/JMSProducer.java
+++ jee/bookservice/app/app-service/src/main/java/com/book/jms/JMSProducer.java
@@ -3,6 +3,7 @@ package com.book.jms;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSContext;
 import javax.jms.JMSException;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
@@ -26,17 +27,32 @@ public class JMSProducer {
                     .lookup("jms/test/ConnectionFactory");
             Destination queue = (Destination) jndiContext.lookup("jms/test/Queue");
 
-            //Создает необходимые артефакты для соединения с очередью
-            Connection connection = connectionFactory.createConnection();
-            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            MessageProducer producer = session.createProducer(queue);
+            sendMsgWithClassicApproach(connectionFactory, queue);
+            sendMsgWithNewApproach(connectionFactory, queue);
 
-            // Отправляем текстовое сообщение
-            TextMessage message = session.createTextMessage("Test message is send. Time: " + LocalDateTime.now());
-            producer.send(message);
-            connection.close();
         } catch (NamingException | JMSException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void sendMsgWithClassicApproach(ConnectionFactory connectionFactory, Destination queue) throws JMSException {
+        //Создает необходимые артефакты для соединения с очередью
+        Connection connection = connectionFactory.createConnection();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        MessageProducer producer = session.createProducer(queue);
+
+        // Отправляем текстовое сообщение
+        TextMessage message = session.createTextMessage("Test message is send with classic approach. Time: " + LocalDateTime.now());
+        producer.send(message);
+        connection.close();
+    }
+
+
+    private static void sendMsgWithNewApproach(ConnectionFactory connectionFactory, Destination queue) {
+        try (JMSContext context = connectionFactory.createContext()) {
+            context
+                    .createProducer()
+                    .send(queue, "Test message is send with new approach. Time: " + LocalDateTime.now());
         }
     }
 
