534aa6bf8379a50c44efc957425023c9ac1bbde3
dmitry yolkin <dmitry.yolkin@gmail.com>
2018-05-15 09:19:20 +0400

    HomeController - small refactopring

--- spring-fund/src/main/java/web/spittr/web/HomeController.java
+++ spring-fund/src/main/java/web/spittr/web/HomeController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author dmitry.yolkin (dmitry.yolkin@maxifier.com) (14.05.18)
  */
 @Controller
+@RequestMapping(value = {"/", "/homepage"})
 public class HomeController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String home(){
         //return View with name 'home' - according to ViewResolver specified at ViewResolver
         //this view will be searched in WEB-INF/views
