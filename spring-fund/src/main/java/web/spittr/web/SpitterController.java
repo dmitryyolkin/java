60b6b70c08d5e9783b6d131d4efbc2e1b4f79ea3
dmitry yolkin <dmitry.yolkin@gmail.com>
2018-06-14 08:55:06 +0400

    Redirect examples: Flash attribute (session survive)

--- spring-fund/src/main/java/web/spittr/web/SpitterController.java
+++ spring-fund/src/main/java/web/spittr/web/SpitterController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import web.spittr.web.dto.Spitter;
 
 import javax.servlet.http.Part;
@@ -38,7 +39,7 @@ public class SpitterController {
     public String processRegister(
             @RequestPart("profilePicture") Part profilePicture, // byte[] can be used instead of Part
             @Valid Spitter spitter,
-            Model model,
+            RedirectAttributes model,
             Errors errors) {
 
         if (errors.hasErrors()) {
@@ -56,6 +57,12 @@ public class SpitterController {
             model.addAttribute("username", spitter.getUserName());
             model.addAttribute("spitterId", spitter.getId());
 
+            // flash attributes survives after redirect
+            // they put into a session before redirect
+            // and restored from the session to a model after redirect
+            // If we don't put spitter in Flash attr then by default it can not be converted to query string
+            model.addFlashAttribute("spitter", spitter);
+
             // as long as mapping to spitterId is absent it will be added as query param
             // smth like /spitter/{username}?spitterId={spitterId}
             return "redirect:/spitter/{username}";
@@ -70,8 +77,11 @@ public class SpitterController {
     public String spitter(
             @PathVariable("username") String username,
             Model model) {
-        Spitter spitter = spittleRepository.findByUsername(username);
-        model.addAttribute("spitter", spitter);
+        Spitter spitter;
+        if (!model.containsAttribute("spitter")) {
+            spitter = spittleRepository.findByUsername(username);
+            model.addAttribute("spitter", spitter);
+        }
         return "profile";
     }
 }
