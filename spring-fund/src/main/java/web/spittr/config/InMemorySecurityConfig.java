11ef24b24205cb41900c721ec1ebc068b1e4659f
dmitry yolkin <dmitry.yolkin@gmail.com>
2018-06-29 09:13:00 +0400

    Create users in jdbc store

--- spring-fund/src/main/java/web/spittr/config/InMemorySecurityConfig.java
+++ spring-fund/src/main/java/web/spittr/config/InMemorySecurityConfig.java
@@ -1,16 +1,21 @@
 package web.spittr.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 /**
  * @author dmitry.yolkin (dmitry.yolkin@maxifier.com) (27.06.18)
  */
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+// Enable if we want to have users stored in memory
+//@Configuration
+//@EnableWebSecurity
+public class InMemorySecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
