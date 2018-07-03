a3c256a74c66c20351cc7f40fcf66b19b9303c1d
dmitry yolkin <dmitry.yolkin@gmail.com>
2018-07-03 09:06:54 +0400

    Example: encode pass stored in DB

--- spring-fund/src/main/java/web/spittr/config/JdbcSecurityConfig.java
+++ spring-fund/src/main/java/web/spittr/config/JdbcSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -37,7 +38,9 @@ public class JdbcSecurityConfig extends WebSecurityConfigurerAdapter {
                 //      "select username,authority from authorities where username = ?"
                 .authoritiesByUsernameQuery(
                         "select username, 'ROLE_USER' from Spitter where username=?"
-                );
+                )
+                // encode password stored in DB
+                .passwordEncoder(new BCryptPasswordEncoder());
 
     }
 }
