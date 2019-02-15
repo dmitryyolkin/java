bf5388463e91a2db23e6ef4011ef05932c5cb683
Dmitry <dmitry.yolkin@maxifier.com>
2019-02-15 09:31:58 +0400

    Add ReadingListController

---
+++ springboot-demo/src/main/java/com/yolkin/springbootdemo/ReadingListController.java
@@ -0,0 +1,51 @@
+package com.yolkin.springbootdemo;
+
+import com.yolkin.springbootdemo.entities.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/")
+public class ReadingListController {
+    private final ReadingListRepository repository;
+
+    @Autowired
+    public ReadingListController(ReadingListRepository repository) {
+        this.repository = repository;
+    }
+
+    // e.g. http://localhost:8080/john.smith
+    @RequestMapping(value = "/{reader}", method = RequestMethod.GET)
+    public String getBooksByReader(
+            @PathVariable("reader") String reader,
+            Model model) {
+
+        List<Book> books = repository.findByReader(reader);
+        if (books != null) {
+            model.addAttribute("books", books);
+        }
+
+        // return name of View that should be shown
+        return "readingList";
+    }
+
+    // e.g. http://localhost:8080/john.smith
+    @RequestMapping(value = "/{reader}", method = RequestMethod.POST)
+    public String addBoo2ReadingList(
+            @PathVariable("reader") String reader,
+            Book book) {
+
+        book.setReader(reader);
+        repository.save(book);
+
+        // redirect to reader's page
+        // it's handled with another controller -
+        return "redirect:/{reader}";
+    }
+}
--- springboot-demo/src/main/java/com/yolkin/springbootdemo/ReadingListRepository.java
+++ springboot-demo/src/main/java/com/yolkin/springbootdemo/ReadingListRepository.java
@@ -2,6 +2,7 @@ package com.yolkin.springbootdemo;
 
 import com.yolkin.springbootdemo.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ public interface ReadingListRepository extends JpaRepository<Book, Long> {
 
     // implementation will be generated automatically in runtime
     // based on methods' names conventions used by SpringData module
+    @Nullable
     List<Book> findByReader(String reader);
 
 }
