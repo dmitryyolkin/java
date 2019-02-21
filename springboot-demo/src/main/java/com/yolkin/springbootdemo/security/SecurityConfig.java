e3ec5f276f17655671a1e05dcc73aa2410dfec29
Dmitry <dmitry.yolkin@maxifier.com>
2019-02-21 10:10:31 +0400

    Add SpringSecurity config

---
+++ springboot-demo/src/main/java/com/yolkin/springbootdemo/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.yolkin.springbootdemo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * By creating this class we ask Spring Boot to skip Basic Spring Security Configuration
+ * and ask to use our own Security config
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ReaderRepository readerRepository;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(username ->
+                        readerRepository
+                                .findById(username)
+                                .orElseThrow(() -> new UsernameNotFoundException("User is not found" + username)));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // require Reader access
+        http
+                .authorizeRequests()
+                .antMatchers("/").access("hasRole('READER')")
+                .antMatchers("/**").permitAll()
+
+                .and()
+
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/login?error=true");
+
+    }
+}
