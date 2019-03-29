3eaac24c5c8df2c5861bff18ff27335ab9ad3c23
Dmitry <dmitry.yolkin@maxifier.com>
2019-03-29 09:58:28 +0400

    Update SecurityConfig

--- springboot-demo/src/main/java/com/yolkin/springbootdemo/security/SecurityConfig.java
+++ springboot-demo/src/main/java/com/yolkin/springbootdemo/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // require Reader access
         http
                 .authorizeRequests()
-                    .antMatchers("/users").access("hasRole('" + Reader.ROLE_READER + "')")
+                    .antMatchers("/").access("hasRole('" + Reader.ROLE_READER + "')")
                     .antMatchers("/**").permitAll()
                 .and()
                     .formLogin()
