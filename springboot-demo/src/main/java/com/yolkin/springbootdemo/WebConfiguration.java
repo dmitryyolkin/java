b5353b9f864b6b68992e7036fb0207383361872f
Dmitry <dmitry.yolkin@maxifier.com>
2019-04-09 09:03:41 +0400

    Replace WebMvcConfigurerAdapter with WebMvcConfigurer

--- springboot-demo/src/main/java/com/yolkin/springbootdemo/WebConfiguration.java
+++ springboot-demo/src/main/java/com/yolkin/springbootdemo/WebConfiguration.java
@@ -4,13 +4,14 @@ import com.yolkin.springbootdemo.security.ReaderHandlerMethodArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
 
 @Configuration
-public class WebConfiguration extends WebMvcConfigurerAdapter {
+public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
